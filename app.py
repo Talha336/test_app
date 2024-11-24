f18a1b1a2ce97d6616fa55c1a656f66c2c8099a3
Talha Toqeer <110573349+Talha336@users.noreply.github.com>
2024-11-24 09:07:10 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+
+# add title
+st.title('Data Analysis Web App')
+st.button('Click Here')
+
+st.write('Hello there')
+
+
+
